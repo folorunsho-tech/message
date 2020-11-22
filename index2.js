1547b1b88638c3b9845657fcc5ea57dd1c448f11
folorunsho-tech <64138997+folorunsho-tech@users.noreply.github.com>
2020-11-22 16:16:55 +0100

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,18 @@
+const { hash } = window.location;
+const message = atob(hash.replace("#", ""));
+if (message) {
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
+  document.querySelector("h1").innerHTML = message;
+}
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const messageInput = document.querySelector("#message-input");
+  const linkInput = document.querySelector("#link-input");
+  const encrypted = btoa(messageInput.value);
+  linkInput.value = `${window.location}#${encrypted}`;
+
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+  linkInput.select();
+});
